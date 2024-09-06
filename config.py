3953dc4f6565876451aca606d3001e4131d06ac2
Kjerry1 <naik29747@gmail.com>
2024-09-06 10:05:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 # Don't use quotes( " and ' )
-#SCRIPT BY VENOMxCRAZY
+#SCRIPT BY JERRYxBOSS
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7501646378:AAF4GybshfRP6nQVQSOFmSP8RJS-8COgLCU")
+BOT_TOKEN=("7204562805:AAEYbOVHH6z-NsRpgFpMkw22YNluVi5MGtg")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("trustetbro11")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("2035820904")
 
 
 
